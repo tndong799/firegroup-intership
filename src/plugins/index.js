8a4c39cd8c53cda973e8d99ce5b2329bcd32c8b5
Đông Trần Ngọc <57401513+ngocdong1999@users.noreply.github.com>
2022-07-22 16:08:00 +0700

    push fe-project to git

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,5 @@
-import Vue from 'vue'
-import mixin from './mixin'
-import VueConfirm from '@/assets/vendor/vue-confirm'
-Vue.mixin(mixin)
-Vue.prototype.$eventBus = new Vue()
+import Vue from 'vue';
+import VueConfirm from '@/assets/vendor/vue-confirm';
+Vue.prototype.$eventBus = new Vue();
 
-Vue.use(VueConfirm)
+Vue.use(VueConfirm);
